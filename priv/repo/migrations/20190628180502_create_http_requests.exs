c674689b7262334b5020667409cd3e090d2e3c12
Ivan Stana <^_^@myrtana.sk>
2019-08-10 11:30:59 +0200

    use text types for fields instead of varchar 255

--- priv/repo/migrations/20190628180502_create_http_requests.exs
+++ priv/repo/migrations/20190628180502_create_http_requests.exs
@@ -3,15 +3,15 @@ defmodule NimbleKitty.Repo.Migrations.CreateHttpRequests do
 
   def change do
     create table(:http_requests) do
-      add :ip_address, :string
+      add :ip_address, :text
       add :time, :utc_datetime_usec
-      add :verb, :string
-      add :url, :string
-      add :version, :string
+      add :verb, :text
+      add :url, :text
+      add :version, :text
       add :status, :integer
       add :response_size, :integer
-      add :user_agent, :string
-      add :referrer, :string
+      add :user_agent, :text
+      add :referrer, :text
 
       timestamps()
     end
