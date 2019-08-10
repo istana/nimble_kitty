c6b4e01e0b8b31898b88ba22cf17fc7954935364
Ivan Stana <^_^@myrtana.sk>
2019-08-10 14:33:34 +0200

    implement top dogs

--- lib/nimble_kitty_web/controllers/page_controller.ex
+++ lib/nimble_kitty_web/controllers/page_controller.ex
@@ -8,7 +8,6 @@ defmodule NimbleKittyWeb.PageController do
   def index(conn, _params) do
     accesses_query =
       from r in HttpRequest,
-        select: count(r.id),
         where:
           not like(r.url, "%.ttf%") and
             not like(r.url, "%.css%") and
@@ -34,42 +33,52 @@ defmodule NimbleKittyWeb.PageController do
 
     last24h_people_query =
       from r in people_query,
+        select: count(r.id),
         where: r.time > ^Timex.shift(Timex.now(), hours: -24)
 
     last48h_people_query =
       from r in people_query,
+        select: count(r.id),
         where: r.time > ^Timex.shift(Timex.now(), hours: -48)
 
     last7d_people_query =
       from r in people_query,
+        select: count(r.id),
         where: r.time > ^Timex.shift(Timex.now(), days: -7)
 
     last30d_people_query =
       from r in people_query,
+        select: count(r.id),
         where: r.time > ^Timex.shift(Timex.now(), days: -30)
 
     last1y_people_query =
       from r in people_query,
+        select: count(r.id),
         where: r.time > ^Timex.shift(Timex.now(), years: -1)
 
     last24h_bots_query =
       from r in bots_query,
+        select: count(r.id),
         where: r.time > ^Timex.shift(Timex.now(), hours: -24)
 
     last48h_bots_query =
       from r in bots_query,
+        select: count(r.id),
         where: r.time > ^Timex.shift(Timex.now(), hours: -48)
 
     last7d_bots_query =
       from r in bots_query,
+        select: count(r.id),
         where: r.time > ^Timex.shift(Timex.now(), days: -7)
 
     last30d_bots_query =
       from r in bots_query,
+        select: count(r.id),
         where: r.time > ^Timex.shift(Timex.now(), days: -30)
 
     last1y_bots_query =
       from r in bots_query,
+        select: count(r.id),
         where: r.time > ^Timex.shift(Timex.now(), years: -1)
 
     last24h_people = Repo.one(last24h_people_query)
@@ -77,16 +86,25 @@ defmodule NimbleKittyWeb.PageController do
     last7d_people = Repo.one(last7d_people_query)
     last30d_people = Repo.one(last30d_people_query)
     last1y_people = Repo.one(last1y_people_query)
-    all_people = Repo.one(people_query)
+    all_people = Repo.one(from r in people_query, select: count(r.id))
 
     last24h_bots = Repo.one(last24h_bots_query)
     last48h_bots = Repo.one(last48h_bots_query)
     last7d_bots = Repo.one(last7d_bots_query)
     last30d_bots = Repo.one(last30d_bots_query)
     last1y_bots = Repo.one(last1y_bots_query)
-    all_bots = Repo.one(bots_query)
+    all_bots = Repo.one(from r in bots_query, select: count(r.id))
 
     total_requests = Repo.one(from r in HttpRequest, select: count(r.id))
+
+    top_dogs = Repo.all(from r in people_query,
+      select: %{
+        url: r.url,
+        visits: count(r.id),
+      },
+      group_by: r.url,
+      order_by: [desc: count(r.id)]
+    )
 
     render(conn, "index.html",
       last24h_people: last24h_people,
@@ -101,7 +119,8 @@ defmodule NimbleKittyWeb.PageController do
       last30d_bots: last30d_bots,
       last1y_bots: last1y_bots,
       all_bots: all_bots,
-      total_requests: total_requests
+      total_requests: total_requests,
+      top_dogs: top_dogs,
     )
   end
 end
